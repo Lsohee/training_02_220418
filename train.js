90c0aad95eb094e7c8b21bdf43befe59e5c1bd39
limsohee <limsohee0521@gmail.com>
2022-04-19 02:17:34 +0900

    서울에서 살때 한달 생활비

---
+++ train.js
@@ -0,0 +1,16 @@
+let house = 350000;
+let firstMonth = 400000;
+let food = 300000;
+let shopping = 150000;
+let basic = 50000;
+let coffee = 80000;
+let heathy = 30000;
+let bus = 30000;
+let hairCut = 30000;
+let play = 80000;
+
+
+
+let nextMonth = house + firstMonth + food + shopping + basic + coffee + heathy + bus + hairCut + play;
+
+console.log(nextMonth);
